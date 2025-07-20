98b0b2a79d7e40e98b4798263f4578aaa989c95f
Ángel Alcántar <rayoalcantar@gmail.com>
2025-07-20 13:03:30 -0600

    se prepara la versión que lo corrije

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.rayoai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
