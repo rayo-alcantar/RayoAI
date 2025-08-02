5f8df06c85cbcc99affe0d8bbfc160f59e0e887e
Ángel Alcántar <rayoalcantar@gmail.com>
2025-08-02 10:37:25 -0600

    se prepara release para google play

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.rayoai"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.3.2"
+        versionCode = 11
+        versionName = "1.3.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
