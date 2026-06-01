e01d7a77a25fc3064a87603ae7557fd498d95eff
Angel Alcantar <rayoalcantar@gmail.comgit>
2026-06-01 10:29:09 -0600

    se prepara pubilcación con la versión 3.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.rayoai"
         minSdk = 29
         targetSdk = 35
-        versionCode = 38
-        versionName = "3.3.0"
+        versionCode = 39
+        versionName = "3.3.1"
         testInstrumentationRunner = "com.rayoai.CustomTestRunner"
         vectorDrawables { useSupportLibrary = true }
     }
